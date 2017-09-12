c995f10fc68e7daa38213a6ebef56687b277e638
Grzegorz Pietrzak <grzegorz.pietrzak@windowslive.com>
2017-09-13 00:03:51 +0200

    FileHandler added, newtonsoft.json reference added

---
+++ MyMiner/MyMiner/FileHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace MyMiner
+{
+    class FileHandler
+    {
+        public static void WriteToFile(string filename, string lines)
+        {
+            File.WriteAllText(@filename, lines);
+        }
+
+        public static string ReadFromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                return File.ReadAllText(filename);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

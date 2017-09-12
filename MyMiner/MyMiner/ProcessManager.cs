07445685809a147f8ab8be015894aa899051084c
Grzegorz Pietrzak <grzegorz.pietrzak@windowslive.com>
2017-09-12 23:57:15 +0200

    ProcessManager added (starts new process and kills running miners)

---
+++ MyMiner/MyMiner/ProcessManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+
+namespace MyMiner
+{
+    class ProcessManager
+    {
+        [STAThread]
+        public static void StartTask(string miner, string param)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = miner,
+                Arguments = param
+            };
+
+            try
+            {
+                Process.Start(startInfo);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public static void EndTask()
+        {
+            var names = new[] { "EthDcrMiner64", "miner", "gominer" };
+
+            foreach (var name in names)
+            {
+                var proccesses = Process.GetProcesses().Where(pr => pr.ProcessName == name);
+                foreach (var process in proccesses)
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
